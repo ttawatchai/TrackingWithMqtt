c0ded2a533d373b1abe7d6233d0ce5d31d96cad6
Tawatchai Job <ttawatchai.job@gmail.com>
2020-06-25 17:43:13 +0700

    First commit

---
+++ trackingwithmqttlibrary/src/main/java/com/ttawatchai/trackingwithmqttlibrary/mqtt/model/MqttResponse.kt
@@ -0,0 +1,17 @@
+package com.ttawatchai.trackingwithmqttlibrary.mqtt.model
+
+data class MqttResponse(
+    val url: String,
+    val port: String,
+    val portSSL: String,
+    val portWS: String,
+    val username: String,
+    val password: String,
+    val isactive: Boolean,
+    val createBy: String,
+    val createDate: String,
+    val updateBy: String,
+    val updateDate: String,
+    val topic: String,
+    val id: Int
+)
